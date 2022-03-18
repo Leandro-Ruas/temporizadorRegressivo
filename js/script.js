ad0cfa886d65a222476da984e66a7415823d9009
Leandro Ruas <you@example.comleandro.ruas.2018@gmail.com>
2022-03-18 17:52:29 -0300

    Criado o arquivo html temporizador responsavel por fazer a contagem, realizdo modificacoes no arquivo index para adicionar dados das horas, realizado a implementacao no script para gerar msg em momentos especificos e execucao do fechamento da tela

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function startTimer(duration, display) {
+function startTimer(duration, display, msg) {
 
     var timer = duration,horas,minutes,seconds;
 
@@ -17,14 +17,34 @@ function startTimer(duration, display) {
             timer = duration;
         }
 
+        if(minutes < 1) {
+
+            msg.textContent = "Daremos iniciou ao nosso culto dentro de instantes.";
+    
+        }
+        
+        if(minutes < 1 && seconds < 30) {
+
+            msg.textContent = "Pedimos para aos amados irmãos tomem seus lugares, pois daremos inicio ao nosso culto";
+
+        }
+
+        
+    if (timer == 0){
+        window.close();
+    }
+
     }, 1000);
 
 }
 
 window.onload = function() {
 
-    var duration = 60 * 60; // conversão para segundos
-    var display = document.querySelector("#timer"); //Elemento para exibir o timer
+    var duration = 60 * 1; // conversão para segundos
+    var display  = document.querySelector("#timer"); //Elemento para exibir o timer
+    var msg      = document.querySelector("#msg");
 
-    startTimer(duration, display); //Inicia a função
+    startTimer(duration, display,msg); //Inicia a função
+
 }
+
